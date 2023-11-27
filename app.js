5df6253f2dd11379085a15de11893614e79f0bb2
1115yiming <yimingzhuzhu@gmail.com>
2023-11-27 16:30:06 -0500

    node server connected

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import express from 'express';
-import Hello from "./hello.js";
-import Lab5 from "./Lab5.js";
-import CourseRoutes from "./courses/routes.js";
+// const express = require('express')
+// import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from "./assignments/routes.js";
 import cors from "cors";
@@ -11,11 +10,10 @@ import "dotenv/config";
 const app = express();
 app.use(cors());
 app.use(express.json());
-ModuleRoutes(app);
-AssignmentRoutes(app);
-CourseRoutes(app);
-Lab5(app);
-Hello(app)
+// nod
+app.get('/hello', (req, res) => {res.send('Hello World!')})
+// CourseRoutes(app);node
+
 
 app.listen(process.env.PORT || 4000)
 
