14035d25239c96a96da7310b830ee0e17f3acc99
jinpengchen-cyber <chen.jinp@northeastern.edu>
2023-12-06 15:52:17 -0500

    address

--- addresses/routes.js
+++ addresses/routes.js
@@ -1,51 +1,61 @@
 import Database from "../Database/index.js";
 
 function AddressRoutes(app) {
+
+    const getUserAddresses = (uid) => Database.addresses.filter(a => a.userId === uid);
+
     app.get("/api/addresses", (req, res) => {
         res.send(Database.addresses);
-    }
-    );
+    });
 
     app.get("/api/users/:uid/addresses", (req, res) => {
         const { uid } = req.params;
-        const addresses = Database.addresses
-            .filter((a) => a.userId === uid);
-        res.send(addresses);
+        res.send(getUserAddresses(uid));
+    });
+
+    app.get("/api/users/:uid/addresses/:aid", (req, res) => {
+        const { uid, aid } = req.params;
+        const userAddresses = getUserAddresses(uid);
+        const address = userAddresses.find(a => a.addressId === aid);
+        res.send(address);
     }
-);
+    
+        );
 
     app.delete("/api/addresses/:aid", (req, res) => {
         const { aid } = req.params;
-        Database.addresses = Database.addresses.filter((a) => a._id !== aid);
+        Database.addresses = Database.addresses.filter(a => a.addressId !== aid);
         res.sendStatus(200);
     });
 
     app.put("/api/addresses/:aid", (req, res) => {
         const { aid } = req.params;
-        const addressIndex = Database.addresses.findIndex((a) => a._id === aid);
-        Database.addresses[addressIndex] = {
-            ...Database.addresses[addressIndex],
-            ...req.body
-        };
-        res.sendStatus(204);
+        const addressIndex = Database.addresses.findIndex(a => a.addressId === aid);
+        if (addressIndex !== -1) {
+            Database.addresses[addressIndex] = { ...Database.addresses[addressIndex], ...req.body };
+            res.sendStatus(204);
+        } else {
+            res.sendStatus(404); // Address not found
+        }
+    });
+
+    app.get("/api/addresses/:aid", (req, res) => {
+        const { aid } = req.params;
+        const address = Database.addresses.find(a => a.addressId === aid);
+        res.send(address);
     });
 
     app.post("/api/users/:uid/addresses", (req, res) => {
         const { uid } = req.params;
         const newAddress = {
             ...req.body,
-            user: uid,
-            _id: 'address' + new Date().getTime().toString(),
+            userId: uid,
+            _id: `address${new Date().getTime()}`,
         };
         Database.addresses.push(newAddress);
-        res.send(newAddress);
+        res.status(201).send(newAddress); // 201 for resource creation
     });
 
-    app.get("/api/users/:uid/addresses", (req, res) => {
-        const { uid } = req.params;
-        const addresses = Database.addresses
-            .filter((a) => a.user === uid);
-        res.send(addresses);
-    });
 }
+
 export default AddressRoutes;
