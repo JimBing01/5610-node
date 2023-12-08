dafe308006619ef8ba0a4bd1920b06a5cb425245
Zhen Lu <zhenlu@ZhendeMacBook-Air.local>
2023-12-08 13:46:53 -0500

    revise searchbar

--- SearchBar/routes.js
+++ SearchBar/routes.js
@@ -1,17 +1,18 @@
 import db from "../Database/index.js";
 
 function SearchBarRoutes(app) {
-	app.get("/api/sandwiches/search", (req, res) => {
-		const query = req.query.name;
-		const matchingSandwiches = db.AllSandwiches.filter((s) =>
-			s.name.toLowerCase().startsWith(query.toLowerCase())
-		);
-		res.send(matchingSandwiches);
-	});
+    app.get("/api/sandwiches/search", (req, res) => {
+        const query = req.query.name;
+        const matchingSandwiches = db.AllSandwiches.filter((s) =>
+            s.name.toLowerCase().includes(query.toLowerCase())
+        );
+        res.send(matchingSandwiches);
+    });
 
-	app.get("/api/sandwiches", (req, res) => {
-		res.send(db.AllSandwiches);
-	});
+    app.get("/api/sandwiches", (req, res) => {
+        res.send(db.AllSandwiches);
+    });
 }
+
 
 export default SearchBarRoutes;
