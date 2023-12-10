f0cc218ade06a5bdca2f49a3b8a07e89c66effe2
Jianping Bai <bai.jian@northeastern.edu>
2023-12-10 15:55:38 -0500

    .

--- Database/index.js
+++ Database/index.js
@@ -6,7 +6,7 @@ import reviews from "./Reviews.json" assert { type: "json" };
 import customerOrder from "./customerOrder.json" assert { type: "json" };
 import shoppingCart from "./shoppingCart.json" assert { type: "json" };
 import AllSandwiches from "./Sandwiches/AllSandwiches.json" assert { type: "json" };
-import addresses from "./Addresses.json" assert { type: "json" };
+import addresses from "./addresses.json" assert { type: "json" };
 import payments from "./payments.json" assert { type: "json" };
 
 
