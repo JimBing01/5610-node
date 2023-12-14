562b0e66aeb50bca4af9a5cf9302eb4815fb96e8
Jianping Bai <bai.jian@northeastern.edu>
2023-12-14 02:23:06 -0500

    .

--- sandwiches/reviews/routes.js
+++ sandwiches/reviews/routes.js
@@ -1,45 +1,55 @@
 import db from "../../Database/index.js";
+import * as daoCustomer from "../../order/dao.js";
+import * as dao from "./dao.js";
 
 function SandwichReviews(app) {
     app.get("/api/sandwiches/:sId/reviews", (req, res) => {
         const sandwichId = req.params.sId;
         const {frontSandwich} = req.query
 
-        const customerOrder = db.customerOrder;
-        const sandwich = db.reviews.find(s => s._id === sandwichId);
-        sandwich.reviews = [];
+        const customerOrderQuery = daoCustomer.findAll();
+        let customerOrder;
+        customerOrderQuery.then(docs => {
+            customerOrder = docs; // docs is an array of documents
+            const sandwich = dao.findBySandwichId(sandwichId);
+            sandwich.reviews = [];
 
-        for(let i = 0; i < customerOrder.length; i++) {
-            let food = customerOrder[i].food;
-            for(let j = 0; j < food.length; j++) {
-                if(food[j][1].name == frontSandwich.name) {
-                    let review = {"body":food[j][1].comment,"username":customerOrder[i].userName,
-                    "userId":customerOrder[i].userId,"date":customerOrder[i].date}
-                    if(food[j][1].star1 == true) {
-                        review = {...review,"rating":1}
-                    } else if(food[j][1].star2 == true) {
-                        review = {...review,"rating":2}
-                    }else if(food[j][1].star3 == true) {
-                        review = {...review,"rating":3}
-                    }else if(food[j][1].star4 == true) {
-                        review = {...review,"rating":4}
-                    }else if(food[j][1].star5 == true){
-                        review = {...review,"rating":5}
-                    }else{
-                        break;
+            for(let i = 0; i < customerOrder.length; i++) {
+                let food = customerOrder[i].food;
+                for(let j = 0; j < food.length; j++) {
+                    if(food[j][1].name == frontSandwich.name) {
+                        let review = {"body":food[j][1].comment,"username":customerOrder[i].userName,
+                            "userId":customerOrder[i].userId,"date":customerOrder[i].date}
+                        if(food[j][1].star1 == true) {
+                            review = {...review,"rating":1}
+                        } else if(food[j][1].star2 == true) {
+                            review = {...review,"rating":2}
+                        }else if(food[j][1].star3 == true) {
+                            review = {...review,"rating":3}
+                        }else if(food[j][1].star4 == true) {
+                            review = {...review,"rating":4}
+                        }else if(food[j][1].star5 == true){
+                            review = {...review,"rating":5}
+                        }else{
+                            break;
+                        }
+                        sandwich.reviews.push(review)
                     }
-                    sandwich.reviews.push(review)
                 }
             }
-        }
+
+
+            if (!sandwich) {
+                res.status(404).json({ error: "Sandwich not found" });
+                return;
+            }
+            res.json(sandwich.reviews || []);
+        }).catch(err => {
+            console.error(err); // Error handling
+        })
 
 
 
-        if (!sandwich) {
-            res.status(404).json({ error: "Sandwich not found" });
-            return;
-        }
-        res.json(sandwich.reviews || []);
     });
 }
 
